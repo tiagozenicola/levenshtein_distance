fb99822f95b107301e03d34870d6c48c89baf4b1
Tiago Zenicola <tiago.zenicola@m4u.com.br>
2016-07-12 19:59:03 -0300

    criação do modelo

---
+++ Levenshtein/src/com/levenshtein/model/Product.java
@@ -0,0 +1,23 @@
+package com.levenshtein.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Product {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+}
