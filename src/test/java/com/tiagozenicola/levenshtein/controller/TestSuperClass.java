55110682e1c76f29bfca4e6d003dba8e0174f513
Tiago Lopes Zenicola <tiagozenicola@gmail.com>
2019-01-12 18:46:43 -0200

    Fix import problem

--- src/test/java/com/tiagozenicola/levenshtein/controller/TestSuperClass.java
+++ src/test/java/com/tiagozenicola/levenshtein/controller/TestSuperClass.java
@@ -14,7 +14,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.tiagozenicola.google.gson.Gson;
+import com.google.gson.Gson;
 import com.tiagozenicola.levenshtein.Application;
 import com.tiagozenicola.levenshtein.model.Product;
 
